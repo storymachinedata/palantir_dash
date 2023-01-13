306df0842ee855969ea135a1aa883bd003d72c35
Sundar Sitaula <storymachinedata@gmail.com>
2023-01-13 16:50:17 +0100

    palantir dash first draft

---
+++ helpers.py
@@ -0,0 +1,144 @@
+import pandas as pd
+import streamlit as st
+from datetime import datetime
+import re
+
+
+
+
+
+month = datetime.today().month
+day = datetime.today().day
+
+
+#files
+
+palantir_path = 'https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/xv0G0VRws9pU6ZsMf1uY2g/palantir.csv'
+paula_cipi_path = 'https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/qmmv9cspSmqDQpn0QoRDdg/paula_cipi.csv'
+jan_hie_path = 'https://phantombuster.s3.amazonaws.com/UhrenaxfEnY/DpKxT9bGGT1HtVTj8BoFnA/jan_hiesserich.csv'
+
+
+
+
+
+storymch_logo = "https://storymachine.mocoapp.com/objects/accounts/a201d12e-6005-447a-b7d4-a647e88e2a4a/logo/b562c681943219ea.png"
+
+
+filters = { 'Total Interaction: High to Low' : ['Total Interactions', False],
+            'Total Interaction: Low to High' : ['Total Interactions', True],
+            'Posts: Newest First': ['date',False],
+            'Posts: Oldest First': ['date',True]}
+
+
+
+
+def read_file(filename):
+    df =pd.read_csv(filename)
+    df = df.dropna(how='any', subset=['textContent'])
+    df.drop(['connectionDegree', 'timestamp'], axis=1, inplace=True)
+    df['postDate'] = df.postUrl.apply(getActualDate)
+    df = df.dropna(how='any', subset=['postDate'])
+    df['date'] =  pd.to_datetime(df['postDate'])
+    df.drop_duplicates(subset=['postUrl'], inplace=True)
+    df = df.reset_index(drop=True)
+    df['Total Interactions'] = df['likeCount'] + df['commentCount']
+    df['likeCount'] = df['likeCount'].fillna(0)
+    df['commentCount'] = df['commentCount'].fillna(0)
+    df['Total Interactions'] = df['Total Interactions'].fillna(0)
+    df['likeCount'] = df['likeCount'].astype(int)
+    df['commentCount'] = df['commentCount'].astype(int)
+    df['Total Interactions'] = df['Total Interactions'].astype(int)
+    df['Keyword']  = df['category']
+    df['yy-dd-mm'] = pd.to_datetime(df.date).dt.strftime('%Y/%m/%d')
+    
+    return df
+
+
+
+
+
+def getActualDate(url):
+    a= re.findall(r"\d{19}", url)
+    a = int(''.join(a))
+    a = format(a, 'b')
+    first41chars = a[:41]
+    ts = int(first41chars,2)
+    actualtime = datetime.fromtimestamp(ts/1000).strftime("%Y-%m-%d %H:%M:%S %Z")
+    return actualtime
+
+
+
+def printFunction(i, rows, dataframe):
+    if not pd.isnull(rows['profileImgUrl']):
+        st.image(rows['profileImgUrl'], width=150)
+
+    if not pd.isnull(rows['profileUrl']):
+        #st.image(rows['profileImgUrl'], width=150)
+        st.subheader(dataframe.fullName[i])
+        st.write('Personal Account')
+        st.write(rows['title']) #postType
+        st.write('-----------')
+        if not pd.isnull(rows['postImgUrl']):
+            st.image(rows['postImgUrl'])
+        st.info(rows['textContent'])  #postrowsontent
+        st.write('Total Interactions 📈:  ',rows['Total Interactions']) #totInterarowstions
+        st.write('Likes 👍:  ',rows['likeCount']) #totInterarowstions
+        st.write('Comments 💬:  ',rows['commentCount']) #totInterarowstions
+        #st.write('Arowstion 📌:  ',rows['arowstion']) #totInterarowstions
+        st.write('Publish Date & Time 📆:         ',rows['postDate']) #publishDate
+        with st.expander('Link to this Post 📮'):
+                st.write(rows['postUrl']) #linktoPost
+        with st.expander('Link to  Profile 🔗'):
+                st.write(rows['profileUrl']) #linktoProfile
+    
+    if not pd.isnull(rows['logoUrl']):
+        st.image(rows['logoUrl'], width=150)
+        st.subheader(rows['companyName'])
+        st.write('Corporate Account')
+        st.write('👥:  ',rows['followerCount'])
+        if not pd.isnull(rows['postImgUrl']):
+            st.image(rows['postImgUrl'])
+        st.info(rows['textContent'])  #postrowsontent
+        st.write('Total Interactions 📈:  ',rows['Total Interactions']) #totInterarowstions
+        st.write('Likes 👍:  ',rows['likeCount']) #totInterarowstions
+        st.write('Comments 💬:  ',rows['commentCount']) #totInterarowstions
+        #st.write('Arowstion 📌:  ',rows['arowstion']) #totInterarowstions
+        st.write('Publish Date & Time 📆:         ',rows['postDate']) #publishDate
+        with st.expander('Link to this Post 📮'):
+                st.write(rows['postUrl']) #linktoPost
+        with st.expander('Link to  Company Profile 🔗'):
+                st.write(rows['companyUrl']) #linktoProfile
+
+
+def printError():
+    st.image('https://img.freepik.com/premium-vector/hazard-warning-attention-sign-with-exclamation-mark-symbol-white_231786-5218.jpg?w=2000', width =200)
+    st.subheader('Oops... No new post found in last Hours.')
+
+
+def printAccountInfo(dataframe, option):
+    dataframe_copy = dataframe[dataframe.Branche == option]
+    dataframe_copy = dataframe_copy.reset_index(drop=True)
+    num_post = dataframe_copy.shape[0]
+    if num_post>0:
+        splits = dataframe_copy.groupby(dataframe_copy.index//3)
+        for _,frame in splits:
+            frame = frame.reset_index(drop=True)
+            thumbnail = st.columns(frame.shape[0])
+            for i, row in frame.iterrows():
+                with thumbnail[i]:
+                    st.subheader(row['Account_Name'])
+                    if not pd.isnull(row['imgUrl']):
+                        st.image(row['imgUrl'])
+                    st.info(row['postContent'])
+                    st.write('Publish Date & Time 📆:         ',row['postDate'])
+                    st.write('Total Interactions 📈:  ',row['Total Interactions'])
+                    st.write('Likes 👍:  ',row['likeCount']) #totInteractions
+                    st.write('Comments 💬:  ',row['commentCount']) #totInteractions
+                    with st.expander('Link to this Post 📮'):
+                        st.write(row['postUrl']) #linktoPost
+                    with st.expander('Link to  Profile 🔗'):
+                        st.write(row['profileUrl']) #linktoProfile
+    else:
+        st.image('https://img.freepik.com/premium-vector/hazard-warning-attention-sign-with-exclamation-mark-symbol-white_231786-5218.jpg?w=2000', width =200)
+        st.subheader('Oops... No new post found for the selection.')
+
---
+++ main.py
@@ -0,0 +1,121 @@
+import numpy as np
+import pandas as pd
+import streamlit as st
+import time
+import datetime as dt
+
+from helpers import *
+
+
+
+st.set_page_config(layout='wide')
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+
+
+logo, _ = st.columns(2)
+with logo:
+	st.image(storymch_logo, width=200)
+   
+st.markdown("<h1 style='text-align: center; color: green;'>Palantir: Community Management Dashboard</h1>", unsafe_allow_html=True)
+
+
+
+
+## Filters to filter the dataframe
+
+col1, col2, col3 = st.columns(3)
+
+with col1:
+	filter_day = st.number_input("Enter number to view posts for that number of days", min_value=1, max_value=30, value=7, step=1)
+	if filter_day:
+		st.success(f'Showing Posts from last {int(filter_day)} Days', icon="✅")
+
+with col2:
+	filter_Interactions = st.selectbox( "Filter by total interactions",
+						('Total Interaction: High to Low',
+						'Total Interaction: Low to High'))
+
+with col3:
+	filter_date = st.selectbox( "Filter by total Post date",
+						('Posts: Newest First',
+						'Posts: Oldest First'))
+
+
+
+
+palantir_df_main = read_file(palantir_path)
+paula_cipi_df_main = read_file(paula_cipi_path)
+jan_hi_df_main = read_file(jan_hie_path)
+
+palantir_df = palantir_df_main[palantir_df_main['date']>=(dt.datetime.now()-dt.timedelta(days=filter_day))]
+paula_cipi_df = paula_cipi_df_main[paula_cipi_df_main['date']>=(dt.datetime.now()-dt.timedelta(days=filter_day))]
+jan_hi_df = jan_hi_df_main[jan_hi_df_main['date']>=(dt.datetime.now()-dt.timedelta(days=filter_day))]
+
+
+palantir_df = palantir_df.sort_values(by = ['yy-dd-mm','Total Interactions'], ascending=[ filters[filter_date][1], filters[filter_Interactions][1]])
+paula_cipi_df = paula_cipi_df.sort_values(by = ['yy-dd-mm','Total Interactions'], ascending=[ filters[filter_date][1], filters[filter_Interactions][1]])
+jan_hi_df = jan_hi_df.sort_values(by = ['yy-dd-mm','Total Interactions'], ascending=[ filters[filter_date][1], filters[filter_Interactions][1]])
+
+
+Palantir, Paula_Cipi, Jan_Hiesserich = st.tabs(['Palantir', 'Paula Cipierre', 'Jan Hiesserich'])
+
+
+with Palantir:
+
+	palantir_df_copy = palantir_df.reset_index(drop=True)
+	num_posts = palantir_df_copy.shape[0]
+	st.write(f'Total number of posts found: ', str(num_posts))
+
+	if  num_posts>0:
+		splits = palantir_df_copy.groupby(palantir_df_copy.index // 3)
+		for _, frames in splits:
+			frames = frames.reset_index(drop=True)
+			thumbnails = st.columns(frames.shape[0])
+			for i, c in frames.iterrows():
+				with thumbnails[i]:
+					printFunction(i, c, frames)               
+	else:
+		printError()
+
+
+with Paula_Cipi:
+	
+
+	paula_cipi_df = paula_cipi_df.reset_index(drop=True)
+	paula_cipi_copy = paula_cipi_df.copy()
+	num_posts = paula_cipi_copy.shape[0]
+	st.write(f'Total number of posts found: ', str(num_posts))
+	if  num_posts>0:
+		splits = paula_cipi_copy.groupby(paula_cipi_copy.index // 3)
+		for _, frames in splits:
+			frames = frames.reset_index(drop=True)
+			thumbnails = st.columns(frames.shape[0])
+			for i, c in frames.iterrows():
+				with thumbnails[i]:
+					printFunction(i, c, frames)               
+	else:
+		printError()
+
+
+
+with Jan_Hiesserich:
+	
+	jan_hi_df = jan_hi_df.reset_index(drop=True)
+	jan_hi_df_copy = jan_hi_df.copy()
+	num_posts = jan_hi_df_copy.shape[0]
+	st.write(f'Total number of posts found: ', str(num_posts))
+
+	if  num_posts>0:
+		splits = jan_hi_df_copy.groupby(jan_hi_df_copy.index // 3)
+		for _, frames in splits:
+			frames = frames.reset_index(drop=True)
+			thumbnails = st.columns(frames.shape[0])
+			for i, c in frames.iterrows():
+				with thumbnails[i]:
+					printFunction(i, c, frames)               
+	else:
+		printError()
+
+
